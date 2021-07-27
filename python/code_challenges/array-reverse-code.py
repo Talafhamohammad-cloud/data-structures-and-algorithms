a55bcede151deb76f9a34a065208b7b175fe4cc3
Talafhamohammad-cloud <josephtelfah@icloud.com>
2021-07-27 15:07:19 +0300

    follw sa'ed comment and the code requierments

--- python/code_challenges/array-reverse-code.py
+++ python/code_challenges/array-reverse-code.py
@@ -1,10 +1,9 @@
 
-array = []
-n=int(input("enter the number of elements"))
-for i in range(n):
-    numbers=int(input("enter the number"))
-    array.append(numbers)
-else: print('you are done')
-print(f'your array is {array}')
-print(f'your reversed array is {array[::-1]} ')
+arr = [1,2,3,4,5,6,7,8,9]
+def reverseArray(arr):
+  revarray=arr[::-1]
+  return revarray
+
+print(f'your array was {arr}')
+print(f'your reversed array is {reverseArray(arr)} ')
 
